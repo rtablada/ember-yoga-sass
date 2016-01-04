cb1b58939bca9965ed252389156772e8772f74a1
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-04 14:20:21 -0600

    Basic Addon Implementation

---
+++ blueprints/ember-yoga-sass/index.js
@@ -0,0 +1,20 @@
+/*jshint node:true*/
+var RSVP  = require('rsvp');
+var chalk = require('chalk');
+
+module.exports = {
+  description: 'install ember-material-lite into a typical project',
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+
+  beforeInstall: function(options) {
+    return RSVP.all([
+      this.addPackageToProject('yoga-sass'),
+    ]);
+  },
+
+  afterInstall: function() {
+    return RSVP.all([
+      this.addPackageToProject('ember-cli-sass', '^4.2.1'),
+    ]);
+  }
+};
