e5fcd7f31983e13ca5ab847da331bd5dce4e3e48
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-04 14:53:03 -0600

    Use Bourbon style tree merging

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-yoga-sass'
+  name: 'ember-yoga-sass',
+
+  treeForStyles: function() {
+    var yogaPath = path.join('node_modules', 'yoga-sass');
+    var yogaTree = new Funnel(this.treeGenerator(yogaPath), {
+      srcDir: '/assets',
+      destDir: '/app/styles',
+    });
+
+    return yogaTree;
+  },
 };
